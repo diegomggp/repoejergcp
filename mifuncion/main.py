c523d14a6ec0c163d90ff7865926545c6da6f274
diego <diegomg456@gmail.com>
2023-11-16 16:38:08 +0100

    subo

--- mifuncion/main.py
+++ mifuncion/main.py
@@ -1,7 +1,19 @@
-def mifuncion(event, context):
-    resultado = "Hola mundo"
+import json
+from google.cloud import storage
+from google.cloud import firestore
 
-    # Imprimir el resultado
-    print(resultado)
+storage_client = storage.Client()
+firestore_client = firestore.Client()
 
-  
+def hello_gcs(event, context):
+    
+    # Descarga el archivo desde Cloud Storage
+    blob = storage_client.bucket(event["bucket"]).get_blob(event["name"])
+    content = blob.download_as_text()
+    file_name = event['name']
+    document_name = file_name.split('.')[0]
+    # Lee y muestra el contenido del archivo JSON
+    data = json.loads(content)
+    print(event)
+    print(data)
+    firestore_client.collection("diegocol").document(document_name).set(data)
